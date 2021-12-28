4f9b3acbc3e4ae4650f763910f4daea4d43fb4ba
SzymonJankowski98 <jankowski.szymon.98@gmail.com>
2021-12-28 19:30:49 +0100

    Fix heroku settings

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,11 @@
 from .base import *
+import django_heroku
 import os
 
 if os.environ.get("ENV") == 'production':
     from .production import *
 elif os.environ.get("ENV") == 'staging':
     from .staging import *
+    django_heroku.settings(locals())
 else:
     from .development import *
