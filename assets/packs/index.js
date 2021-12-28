e8b1dc75390a1eed13c1f78ffc03410fdf69c51c
SzymonJankowski98 <jankowski.szymon.98@gmail.com>
2021-12-28 18:55:42 +0100

    Add alerts

--- assets/packs/index.js
+++ assets/packs/index.js
@@ -1,3 +1,7 @@
+import '../javascript/alerts'
+
 import '../style/tailwind.css';
 import '../style/fonts.css';
 import '../style/custom_scroll.css';
+import '../style/alert.css';
+import '../style/main.css';
