524cad42d52eb8d2f40fd1683f9f16be6941f5cb
DESKTOP-9C98KHC\pgumo <p.gumowski38@gmail.com>
2021-12-29 18:01:34 +0100

    Add placeholders to job form

--- computing_environment/forms/job.py
+++ computing_environment/forms/job.py
@@ -1,8 +1,12 @@
 from django import forms
-from django.forms import fields
+from django.forms import fields, widgets
 from ..models import Job
 
 class JobForm(forms.ModelForm):
     class Meta:
         model = Job
         fields = ['name', 'program', 'settings', 'is_private']
+        widgets = {
+            'name': widgets.TextInput(attrs={"placeholder": "Job Name"}),
+            'settings': widgets.Textarea(attrs={ "placeholder": '{\n"setting": "value"\n}'})
+        }
