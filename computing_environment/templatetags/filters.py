d642b5506fd3d7c0476b4731b69cfd47949bedc7
unknown <frieske.jakub@gmail.com>
2021-12-30 21:46:17 +0100

    Fix paggination and download

--- computing_environment/templatetags/filters.py
+++ computing_environment/templatetags/filters.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter(name='addclass')
 def addclass(value, arg):
     return value.as_widget(attrs={'class': arg})
+
+
+@register.filter(name='only_name')
+def only_name(file):
+    return file.split('/')[-1]
