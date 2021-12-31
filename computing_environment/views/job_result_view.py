85067d584210eff79168e1a65fc7b278b462ab26
unknown <frieske.jakub@gmail.com>
2021-12-31 13:31:07 +0100

    Separate views

--- computing_environment/views/job_result_view.py
+++ computing_environment/views/job_result_view.py
@@ -1,16 +1,11 @@
-from django.contrib.auth.decorators import login_required
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-from computing_environment.models import JobResult, Job
+from computing_environment.models import JobResult
 
 from ..serializers import JobResultSerializer
 from ..constants import JobStates
-from django.http import HttpResponse
-import os
-from django.core.exceptions import PermissionDenied
-from django.shortcuts import get_object_or_404
 
 @api_view(['POST'])
 def send_result(request):
@@ -27,14 +22,3 @@ def send_result(request):
         return Response(content, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
     return Response(job_result_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@login_required
-def download(request, id):
-    result = get_object_or_404(JobResult,pk=id)
-    job = result.job
-    if job.is_private and job.creator != request.user:
-        raise PermissionDenied()
-    file_buffer = open(result.result.path, "rb").read()
-    response = HttpResponse(file_buffer, content_type='application/force-download')
-    response['Content-Disposition'] = 'attachment; filename=' + os.path.basename(result.result.path)
-    return response
