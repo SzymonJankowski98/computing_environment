5d7f8ff030561fa68a2343a957aecd4823c4dec9
DESKTOP-9C98KHC\pgumo <p.gumowski38@gmail.com>
2022-01-05 19:52:32 +0100

    Update shadows and colors on active links

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
          },
          colors: {
             'primary': '#4338ca',
+            'primary-brighter': '#6358ea',
             'primary-darker': '#3730a3',
             'primary-disabled': '#9790b3',
             'background': '#e2e8f0',
@@ -18,7 +19,7 @@ module.exports = {
             'accept': '#1ebd10'
          },
          boxShadow: {
-            'inner-xl': 'inset 4px 4px 20px 5px rgba(0,0,0, 0.5)'
+            'inner-xl': 'inset 2px 2px 10px 2px rgba(0,0,0, 0.5)'
          }
       }
    },
