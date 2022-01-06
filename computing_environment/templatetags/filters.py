10b6b8afe67d3ca98b696a60f59fa6e3ccc70cda
unknown <frieske.jakub@gmail.com>
2022-01-06 15:54:35 +0100

    Fix searchbar/results

--- computing_environment/templatetags/filters.py
+++ computing_environment/templatetags/filters.py
@@ -19,10 +19,6 @@ def upto(value, delimiter=None):
 
 @register.simple_tag(takes_context=True)
 def param_replace(context, **kwargs):
-    """
-    Based on
-    https://stackoverflow.com/questions/22734695/next-and-before-links-for-a-django-paginated-query/22735278#22735278
-    """
     d = context['request'].GET.copy()
     for k, v in kwargs.items():
         d[k] = v
