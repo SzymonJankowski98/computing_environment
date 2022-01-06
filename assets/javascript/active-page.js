4928496e77cd0a99461de2d557ae46f4d35db9ba
DESKTOP-9C98KHC\pgumo <p.gumowski38@gmail.com>
2022-01-06 13:45:50 +0100

    Remove useless console.log

--- assets/javascript/active-page.js
+++ assets/javascript/active-page.js
@@ -1,13 +1,9 @@
 window.addEventListener('load', () => { 
-    console.log(window.location.pathname);
     const path = window.location.pathname;
 
     if ( /job\/\d+\/edit/.test(path) ) {
         document.getElementById('job_edit_link').style.display = 'flex';
-        console.log("Job Edit Page Here")
     } else if ( /job\/\d+$/.test(path) ) {
         document.getElementById('job_preview_link').style.display = 'flex';
-        console.log("Job Preview Page Here")
     }
-
 })
