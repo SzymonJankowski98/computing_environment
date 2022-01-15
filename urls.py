771b67479be0f04879f28d7d70affa519d61426d
SzymonJankowski98 <jankowski.szymon.98@gmail.com>
2022-01-15 16:59:48 +0100

    Allow to delete sub task

--- urls.py
+++ urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('job/<int:id>', show_job, name='show_job'),
     path('job/<int:id>/edit', edit_job, name='edit_job'),
     path('job/<int:id>/delete', delete_job, name='delete_job'),
+    path('sub_job/<int:id>/delete', delete_sub_job, name='delete_sub_job'),
     path('profile', edit_profile, name='edit_profile'),
 
     ## allauth
