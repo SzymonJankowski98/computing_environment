8b29349b1a2e10f4b34c1698214119b3c2c49d5f
unknown <frieske.jakub@gmail.com>
2022-01-17 20:44:22 +0100

    Fix all download

--- computing_environment/views/download_view.py
+++ computing_environment/views/download_view.py
@@ -1,9 +1,12 @@
 import os
+from pathlib import Path
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from computing_environment.models.job import Job
 from zipfile import ZipFile
+from django.utils import timezone
+from config.base import MEDIA_ROOT
 
 from computing_environment.models import SubJob
 from django.http import HttpResponse
@@ -22,17 +25,24 @@ def download(request, id):
     response['Content-Disposition'] = 'attachment; filename=' + os.path.basename(result.result.path)
     return response
 
+def save_all_results_directory(id,name):
+    zip_directory ='{0}/combined_results/{1}/'.format(MEDIA_ROOT,id)
+    Path(zip_directory).mkdir(parents=True, exist_ok=True)
+    filename = "{}_{}_{}".format(timezone.now().strftime("%Y-%m-%d_%H%M%S.%f"),name,'all_results.zip')
+    return  os.path.join(zip_directory,filename)
+
 @login_required
 def download_all(request, id):
     job = get_object_or_404(Job,pk=id)
     if job.is_private and job.creator != request.user:
         raise PermissionDenied()
 
-    with ZipFile('all_download.zip', 'w') as zipObj:
+    zip_path = save_all_results_directory(job.id,job.name)
+    
+    with ZipFile(zip_path, 'w') as zipObj:
         for sub in job.get_completed_subtasks():
             zipObj.write(sub.result.path, os.path.basename(sub.result.path))
 
-    response = HttpResponse(open('all_download.zip', "rb").read(), content_type='application/force-download')
-    response['Content-Disposition'] = 'attachment; filename=' + os.path.basename(f"all_download.zip")
-    os.remove('all_download.zip')
+    response = HttpResponse(open(zip_path, "rb").read(), content_type='application/force-download')
+    response['Content-Disposition'] = 'attachment; filename=' + os.path.basename(zip_path)
     return response
